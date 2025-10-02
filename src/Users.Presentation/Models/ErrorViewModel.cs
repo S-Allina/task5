fbf2c3bc4d1069f2f5501a05ef00a0403d20247c
Serova Alina <serovaallina2005@gmail.com>
2025-10-02 20:25:47 +0300

    fix architecture

--- src/Users.Presentation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Users.Presentation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
